ac71852a71af3cd96d9d0cc241632fa7cff6b0d4
santiagoAvellaR <santiagoavellar02@gmail.com>
2024-04-25 17:54:36 -0500

    libreria de JUnit instalada

--- square/src/domain/Square.java
+++ square/src/domain/Square.java
@@ -57,27 +57,104 @@ public class Square {
         squareBoard[row][col] = values[0] + " " +  values[1] + " " + color;
     }
 
-    public void moveUp() throws SquareException {
+    private void moveUp() throws SquareException {
         for (int i = 1; i < boardSize; i++){
             for (int j = 0; j < boardSize; j++){
+                makeMovement(i, j, i-1, j);
+            }
+        }
+    }
+
+    private void moveDown() throws SquareException {
+        for (int i = boardSize-1-1; i >= 0; i--){
+            for (int j = 0; j < boardSize; j++){
+                makeMovement(i, j, i+1, j);
+            }
+        }
+    }
+
+    private void moveRight() throws SquareException {
+        for (int j = 0; j < boardSize-1-1; j++){
+            for (int i = 0; i < boardSize; i++){
+                makeMovement(i, j, i, j+1);
+            }
+        }
+    }
+
+    private void moveLeft() throws SquareException {
+        for (int j = boardSize-1-1; j >= 0; j--){
+            for (int i = 0; i < boardSize; i++){
                 makeMovement(i, j, i, j-1);
             }
         }
     }
 
-    private void makeMovement(int rowIni, int colIni, int rowFin, int colFin) throws SquareException{
-        //  ficha - hueco - hueroRelleno - huecoRellenoFicha
-        if (squareBoard[rowIni][colIni] != null){
-            String[] initialSquareInfo = squareBoard[rowIni][colIni].split(" ");
-            if (initialSquareInfo[1].equals("ficha")) {
-                
-            }
+    public void move(char direction) throws SquareException {
+        if (direction == 'u' || direction == 'U'){
+            moveUp();
+        }
+        else if (direction == 'd' || direction == 'D'){
+            moveDown();
+        }
+        else if (direction == 'r' || direction == 'R'){
+            moveRight();
+        }
+        else if (direction == 'l' || direction == 'L'){
+            moveLeft();
         }
     }
 
-    private void setSquareStatus(int row, int col, boolean isSquare, boolean isHole){
-        String[] squareInfo = squareBoard[row][col].split(" ");
-        squareBoard[row][col] = "square:" + isSquare + " " + "hole:" + isHole + " " + squareInfo[2];
+    private void makeMovement(int iniRow, int iniCol, int finRow, int finCol) throws SquareException{
+        //  ficha - hueco - hueroRelleno - huecoRellenoFicha
+        if(squareBoard[iniRow][finCol] == null){return;}
+        String[] initialSquareInfo= squareBoard[iniRow][finCol].split(" ");
+        if(initialSquareInfo[0].equals("hueco") || initialSquareInfo[0].equals("huecoRelleno")){return;}
+        if (initialSquareInfo[0].equals("ficha")){
+            if (squareBoard[finRow][finCol] == null){
+                squareBoard[finRow][finCol] = squareBoard[iniRow][iniCol];
+                squareBoard[iniRow][iniCol] = null;
+            }
+            else {
+                String[] finalSquareInfo = squareBoard[finRow][finCol].split(" ");
+                if (finalSquareInfo[0].equals("hueco")) {
+                    if(finalSquareInfo[1].equals(initialSquareInfo[1])){
+                        squareBoard[finRow][finCol] = "huecoRelleno" + " " + initialSquareInfo[1];
+                        squareBoard[iniRow][iniCol] = null;
+                    }
+                    else{
+                        gameEnd = true;
+                        throw new SquareException(SquareException.GAME_END);
+                    }
+                }
+                else if (finalSquareInfo[0].equals("huecoRelleno")){
+                    squareBoard[finRow][finCol] = "huecoRelleno" + " " + finalSquareInfo[1] + " " + initialSquareInfo[1];
+                    squareBoard[iniRow][iniCol] = null;
+                }
+            }
+        }
+        else if (initialSquareInfo[0].equals("huecoRellenoFicha")){
+            if (squareBoard[finRow][finCol] == null){
+                squareBoard[finRow][finCol] = "ficha" + " " + initialSquareInfo[2];
+                squareBoard[iniRow][iniCol] = "hueco" + " " + initialSquareInfo[1];
+            }
+            else {
+                String[] finalSquareInfo = squareBoard[finRow][finCol].split(" ");
+                if (finalSquareInfo[0].equals("hueco")) {
+                    if(finalSquareInfo[1].equals(initialSquareInfo[2])){
+                        squareBoard[finRow][finCol] = "huecoRelleno" + " " + initialSquareInfo[2];
+                        squareBoard[iniRow][iniCol] = "huecoRelleno" + " " + initialSquareInfo[1];
+                    }
+                    else{
+                        gameEnd = true;
+                        throw new SquareException(SquareException.GAME_END);
+                    }
+                }
+                else if (finalSquareInfo[0].equals("huecoRelleno")){
+                    squareBoard[finRow][finCol] = "huecoRellenoFicha" + " " + finalSquareInfo[1] + " " + initialSquareInfo[2];
+                    squareBoard[iniRow][iniCol] = "huecoRelleno" + " " + initialSquareInfo[1];
+                }
+            }
+        }
     }
 
 }
---
+++ square/src/test/src/domain/SquareTest.java
@@ -0,0 +1,12 @@
+package src.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquareTest {
+
+    @Test
+    void move() {
+    }
+}
